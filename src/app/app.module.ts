eb8318fe2b5d5db8bb40107a1d4e628a484d710e
Nemanja Pantoš <npantos@gmail.com>
2018-01-09 18:15:59 +0100

    dodavanje layouta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsListComponent
+    ContactsListComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
